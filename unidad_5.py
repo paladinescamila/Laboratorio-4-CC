86e01017e84cada8978691952743880decb9a9b2
paladinescamila <camilapaladines27@gmail.com>
2021-05-05 16:02:37 -0500

    Fix functions error

--- unidad_5.py
+++ unidad_5.py
@@ -10,66 +10,69 @@ x = sym.Symbol("x")
 
 
 # Método de Diferencias Finitas Hacia Adelante
-def diferencia_hacia_adelante(funcion, h):
+def diferencia_hacia_adelante(f, h):
     """
     Entrada: una función f(x) y un entero h.
     Salida: primera derivada f'(x).
     """
-    f = sym.lambdify(x_sym, funcion)
-    derivada = (f(x+h) - f(x)) / h
+
+    derivada = (f.subs(x,x+h) - f) / h
     return derivada
 
 
 # Método de Diferencias Finitas Hacia Atrás
-def diferencia_hacia_atras(funcion, h):
+def diferencia_hacia_atras(f, h):
     """
     Entrada: una función f(x) y un entero h.
     Salida: primera derivada f'(x).
     """
-    f = sym.lambdify(x_sym, funcion)
-    derivada = (f(x) - f(x-h)) / h
+
+    derivada = (f - f.subs(x,x-h)) / h
     return derivada
 
 
 # Método de Diferencia Centrada
-def diferencia_centrada(funcion, h):
+def diferencia_centrada(f, h):
     """
     Entrada: una función f(x) y un entero h.
     Salida: primera derivada f'(x).
     """
-    f = sym.lambdify(x_sym, funcion)
-    derivada = (f(x+h) - f(x-h)) / (2*h)
+
+    derivada = (f.subs(x,x+h) - f.subs(x,x-h)) / (2*h)
     return derivada
 
 
 # Segunda derivada
-def segunda_derivada(funcion, h):
+def segunda_derivada(f, h):
     """
     Entrada: una función f(x) y un entero h.
     Salida: segunda derivada f''(x).
     """
-    f = sym.lambdify(x_sym, funcion)
-    derivada = (f(x+h) -2*f(x) + f(x-h)) / (h**2)
+
+    derivada = (f.subs(x,x+h) -2*f + f.subs(x,x-h)) / (h**2)
     return derivada
 
 
 # Pintar ejemplo de derivada con cada método
-def ejemplo_derivada(funcion, analitica, hs, min_x, min_y, mostrar):
+def ejemplo_derivada(funcion, hs, min_x, min_y, mostrar):
     # FALTA LA SEGUNDA DERIVADA
 
-    x_funcion = np.linspace(min_x, min_y, 1000)
     colores = ["purple", "red", "green", "orange", "gray"]
     metodos = ["Hacia Adelante", "Hacia Atrás", "Centrada"]
-    
-    f_funcion = sym.lambdify(x, funcion)
-    f_analitica = sym.lambdify(x, analitica)
 
+    x_funcion = np.linspace(min_x, min_y, 1000)
+    f_funcion = sym.lambdify(x, funcion)
     y_funcion = [f_funcion(i) for i in x_funcion]
+    
+    analitica = sym.diff(funcion, x)
+    f_analitica = sym.lambdify(x, analitica)
     y_analitica = [f_analitica(i) for i in x_funcion]
 
     if (mostrar):
         print("f(x) = {}".format(funcion))
         print("f'(x) = {}".format(analitica))
+
+    y_metodos = []
 
     for i in range(3):
 
@@ -79,12 +82,13 @@ def ejemplo_derivada(funcion, analitica, hs, min_x, min_y, mostrar):
             plt.title("Diferencias Finitas " + metodos[i])
             plt.plot(x_funcion, y_funcion, color="black", label="f(x)")
             plt.plot(x_funcion, y_analitica, color="blue", label="f'(x)")
+            
+            print("---------------------------------------------")
+            print(" DIFERENCIAS FINITAS {}".format(metodos[i].upper()))
+            print("---------------------------------------------")
+            print(" h\tTiempo\tError (Prom)\tError (Desv)\tf'(x)")
+            print("---------------------------------------------")
 
-            print("------------------------------------------------------------------------")
-            print("                  DIFERENCIAS FINITAS {}                  ".format(metodos[i].upper()))
-            print("------------------------------------------------------------------------")
-            print("h\tTiempo\tError (Prom)\tError (Desv)\tf'(x)")
-            print("------------------------------------------------------------------------")
 
         for j in range(len(hs)):
 
@@ -106,39 +110,61 @@ def ejemplo_derivada(funcion, analitica, hs, min_x, min_y, mostrar):
             f_derivada = sym.lambdify(x, derivada)
             y_derivada = [f_derivada(k) for k in x_funcion]
 
+            if (j == 0): y_metodos += [y_derivada]
+
             errores = [np.abs(y_derivada[k] - y_analitica[k]) for k in range(1000)]
             promedio = np.mean(errores)
             desviacion = np.std(errores)
 
             if (mostrar):
-                print("{}\t{:.4f}\t{:.10f}\t{:.10f}\t{}".format(hs[j], tiempo, promedio, desviacion, sym.expand(derivada)))
+                print(" {}\t{:.4f}\t{:.10f}\t{:.10f}\t{}".format(hs[j], tiempo, promedio, desviacion, sym.expand(derivada)))
                 plt.plot(x_funcion, y_derivada, color=colores[j], label="h = "+str(hs[j]))
 
         if (mostrar):
 
-            print("------------------------------------------------------------------------\n")
+            print("---------------------------------------------\n")
             plt.legend()
             plt.xlabel('x')
             plt.ylabel('y')
             plt.grid()
             plt.show()
+    
+    print()
+    plt.title("Métodos de Diferenciación")
+    plt.plot(x_funcion, y_funcion, color="black", label="f(x)")
+    plt.plot(x_funcion, y_analitica, color="blue", label="f'(x)")
+
+    for i in range(3):
+        plt.plot(x_funcion, y_metodos[i], color=colores[i], label=str(metodos[i]))
+    
+    plt.legend()
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid()
+    plt.show()
+
+    print()
 
 
+# EJEMPLOS DE PRUEBA (También se encuentran en el informe)
 def main():
 
     print("DIFERENCIACIÓN")
 
     print("EJEMPLO 1")
     funcion = 5*x**4 + 10*x**3 + 15*x**2 + x + 7
-    analitica = 20*x**3 + 30*x**2 + 30*x + 1
     hs = [1, 0.2, 0.03]
-    ejemplo_derivada(funcion, analitica, hs, -1, 1, True)
+    ejemplo_derivada(funcion, hs, -1, 1, True)
 
-    # print("EJEMPLO 2")
-    # funcion = sym.sin(x + 3*x**2)
-    # analitica = (6*x + 1) * sym.cos(3*x**2 + x)
-    # hs = [2, 0.4, 0.015]
-    # ejemplo_derivada(funcion, analitica, hs, -1, 1, True)
+    print("EJEMPLO 2")
+    funcion = 3**(x + 2)
+    hs = [1, 0.1, 0.01]
+    ejemplo_derivada(funcion, hs, -0.5, 0.5, True)
+
+    print("EJEMPLO 3")
+    funcion = sym.sin(2*x**3)
+    hs = [1, 0.1, 0.01]
+    ejemplo_derivada(funcion, hs, -1, 1, True)
 
 
 main()
