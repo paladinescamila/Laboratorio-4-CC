8f4472bd870c26fa553f94b38acde4b5c944e501
paladinescamila <camilapaladines27@gmail.com>
2021-05-18 16:41:29 -0500

    Fix samples

--- unidad_5.py
+++ unidad_5.py
@@ -12,7 +12,7 @@ x = sym.Symbol("x")
 def diferencia_hacia_adelante(f, h):
     """
     Entrada: una función f(x) y un real h.
-    Salida: primera derivada f'(x).
+    Salida: primera derivada aproximada f'(x).
     """
 
     derivada = (f.subs(x,x+h) - f) / h
@@ -23,7 +23,7 @@ def diferencia_hacia_adelante(f, h):
 def diferencia_hacia_atras(f, h):
     """
     Entrada: una función f(x) y un real h.
-    Salida: primera derivada f'(x).
+    Salida: primera derivada aproximada f'(x).
     """
 
     derivada = (f - f.subs(x,x-h)) / h
@@ -34,18 +34,18 @@ def diferencia_hacia_atras(f, h):
 def diferencia_centrada(f, h):
     """
     Entrada: una función f(x) y un real h.
-    Salida: primera derivada f'(x).
+    Salida: primera derivada aproximada f'(x).
     """
 
     derivada = (f.subs(x,x+h) - f.subs(x,x-h)) / (2*h)
     return derivada
 
 
-# Segunda derivada
+# Segunda Derivada
 def segunda_derivada(f, h):
     """
     Entrada: una función f(x) y un real h.
-    Salida: segunda derivada f''(x).
+    Salida: segunda derivada aproximada f''(x).
     """
 
     derivada = (f.subs(x,x+h) -2*f + f.subs(x,x-h)) / (h**2)
@@ -56,7 +56,7 @@ def segunda_derivada(f, h):
 def punto_medio(f, a, b, n):
     """
     Entrada: función f(x), dos reales a & b, y un entero n.
-    Salida: integral definida de f(x) con límites a y b, con n paneles.
+    Salida: integral definida aproximada de f(x) con límites a y b, con n paneles.
     """
 
     xi = np.linspace(a, b, n)
@@ -73,7 +73,7 @@ def punto_medio(f, a, b, n):
 def trapezoide(f, a, b, n):
     """
     Entrada: función f(x), dos reales a & b, y un entero n.
-    Salida: integral definida de f(x) con límites a y b con n paneles.
+    Salida: integral definida aproximada de f(x) con límites a y b con n paneles.
     """
 
     xi = np.linspace(a, b, n)
@@ -90,7 +90,7 @@ def trapezoide(f, a, b, n):
 def simpson(f, a, b, n):
     """
     Entrada: función f(x), dos reales a & b, y un entero n.
-    Salida: integral definida de f(x) con límites a y b con n paneles.
+    Salida: integral definida aproximada de f(x) con límites a y b con n paneles.
     """
 
     xi = np.linspace(a, b, n)
@@ -107,22 +107,18 @@ def simpson(f, a, b, n):
 def ejemplo_derivada(funcion, hs, a, b, mostrar):
     """
     Entrada: una función funcion(x), una lista de reales hs, dos reales a & b
-            y un booleano mostrar.
-    Salida: la derivada analítica de la función, las derivadas calculadas por los
-            métodos de diferenciación finita para cada h, los tiempos de ejecución, 
-            los errores promedio y las desviaciones de los errores.
+            y un booleano "mostrar".
+    Salida: la derivada analítica, las derivadas calculadas por los métodos para cada h, 
+            los tiempos de ejecución, los errores promedio y las desviaciones de los errores.
     """
 
     x_funcion = np.linspace(a, b, 1000)
-    f_funcion = sym.lambdify(x, funcion)
-    y_funcion = [f_funcion(i) for i in x_funcion]
     
     analitica = sym.diff(funcion, x)
     f_analitica = sym.lambdify(x, analitica)
     y_analitica = [f_analitica(i) for i in x_funcion]
 
     nh = len(hs)
-    t_metodos, p_metodos, d_metodos, f_metodos, y_metodos = [], [], [], [], []
     derivadas = [[None for _ in range(nh)] for _ in range(3)]
     tiempos = [[None for _ in range(nh)] for _ in range(3)]
     promedios = [[None for _ in range(nh)] for _ in range(3)]
@@ -134,6 +130,7 @@ def ejemplo_derivada(funcion, hs, a, b, mostrar):
         print("f'(x) = {}".format(analitica))
         print("a = {}\nb = {}\nhs = {}".format(a, b, hs))
 
+        t_metodos, p_metodos, d_metodos, f_metodos, y_metodos = [], [], [], [], []
         colores = ["purple", "red", "green", "orange", "gray"]
         metodos = ["Hacia Adelante", "Hacia Atrás", "Centrada"]
         if (nh > 5): colores = ["purple" for _ in range(nh)]
@@ -143,9 +140,6 @@ def ejemplo_derivada(funcion, hs, a, b, mostrar):
         if (mostrar):
 
             plt.title("Diferencias Finitas " + metodos[i])
-            plt.plot(x_funcion, y_funcion, color="black", label="f(x)")
-            plt.plot(x_funcion, y_analitica, color="blue", label="f'(x)")
-            
             print("----------------------------------------------")
             print(" DIFERENCIAS FINITAS {}".format(metodos[i].upper()))
             print("----------------------------------------------")
@@ -176,12 +170,12 @@ def ejemplo_derivada(funcion, hs, a, b, mostrar):
             promedio = np.mean(errores)
             desviacion = np.std(errores)
 
-            if (j == 0):
-                t_metodos += [tiempo]
-                p_metodos += [promedio]
-                d_metodos += [desviacion]
-                f_metodos += [derivada]
-                y_metodos += [y_derivada]
+            if (j == 0 and mostrar):
+                t_metodos.append(tiempo)
+                p_metodos.append(promedio)
+                d_metodos.append(desviacion)
+                f_metodos.append(derivada)
+                y_metodos.append(y_derivada)
             
             derivadas[i][j], tiempos[i][j] = derivada, tiempo
             promedios[i][j], desviaciones[i][j] = promedio, desviacion
@@ -190,11 +184,12 @@ def ejemplo_derivada(funcion, hs, a, b, mostrar):
                 print(" {}\t{:.4f}\t{:.10f}\t{:.10f}\t{}".format(hs[j], tiempo, promedio, desviacion, sym.expand(derivada)))
                 # print("\t\t{} & {:.10f} & {:.10f} & {:.10f} \\\\".format(hs[j], tiempo, promedio, desviacion))
                 # print("\t\t{} & ${}$ \\\\".format(hs[j], sym.expand(derivada)))
-                plt.plot(x_funcion, y_derivada, color=colores[j], label="h = "+str(hs[j]))
+                plt.plot(x_funcion, y_derivada, color=colores[j], label="h = "+str(hs[j]), linewidth=2)
 
         if (mostrar):
 
             print("----------------------------------------------\n")
+            plt.plot(x_funcion, y_analitica, color="black", label="Analítica")
             plt.legend()
             plt.xlabel('x')
             plt.ylabel('y')
@@ -205,9 +200,6 @@ def ejemplo_derivada(funcion, hs, a, b, mostrar):
 
         print()
         plt.title("Métodos de Diferenciación con h = "+str(hs[0]))
-        plt.plot(x_funcion, y_funcion, color="black", label="f(x)")
-        plt.plot(x_funcion, y_analitica, color="blue", label="f'(x)")
-
         print("-----------------------------------------------------")
         print("                DIFERENCIAS FINITAS                 ")
         print("-----------------------------------------------------")
@@ -215,9 +207,12 @@ def ejemplo_derivada(funcion, hs, a, b, mostrar):
         print("-----------------------------------------------------")
         for i in range(3):
             print(" {}\t{:.5f}\t{:.10f}\t{:.10f}\t{}".format(metodos[i], t_metodos[i], p_metodos[i], d_metodos[i], f_metodos[i]))
-            plt.plot(x_funcion, y_metodos[i], color=colores[i], label=str(metodos[i]))
+            # print("\t\t{} & {:.5f} & {:.5f} & {:.5f} \\\\".format(metodos[i], t_metodos[i], p_metodos[i], d_metodos[i]))
+            # print("\t\t{} & ${}$ \\\\".format(hs[j], f_metodos))
+            plt.plot(x_funcion, y_metodos[i], color=colores[i], label=str(metodos[i]), linewidth=2)
         print("-----------------------------------------------------")
         
+        plt.plot(x_funcion, y_analitica, color="black", label="Analítica", linewidth=2)
         plt.legend()
         plt.xlabel('x')
         plt.ylabel('y')
@@ -229,86 +224,48 @@ def ejemplo_derivada(funcion, hs, a, b, mostrar):
 
 
 # Pintar ejemplo de integral con cada método
-def ejemplo_integral(funcion, ns, a, b, mostrar):
+def ejemplo_integral(funcion, n, a, b, mostrar):
     """
-    Entrada: una función funcion(x), una lista de enteros ns, dos reales a & b
-            y un booleano mostrar.
-    Salida: la integral definida analítica de la función, las integrales definidas 
-            calculadas por las reglas de cuadratura compuesta para cada n, los 
-            tiempos de ejecución, los errores promedio y las desviaciones de los errores.
+    Entrada: una función funcion(x), un entero n, dos reales a & b y un booleano "mostrar".
+    Salida: la integral definida analítica, las integrales definidas calculadas por cada 
+            método, los tiempos de ejecución, los errores promedio y las desviaciones de los errores.
     """
     
     analitica = float(sym.integrate(funcion, (x, a, b)))
+    integrales, tiempos, errores = [], [], []
 
-    nn = len(ns)
-    t_metodos, e_metodos, f_metodos = [], [], []
-    integrales = [[None for _ in range(nn)] for _ in range(3)]
-    tiempos = [[None for _ in range(nn)] for _ in range(3)]
-    errores = [[None for _ in range(nn)] for _ in range(3)]
+    inicio = time.time()
+    integrales.append(punto_medio(funcion, a, b, n))
+    tiempos.append(time.time() - inicio)
+    errores.append(np.abs(analitica - integrales[0]))
+
+    inicio = time.time()
+    integrales.append(trapezoide(funcion, a, b, n))
+    tiempos.append(time.time() - inicio)
+    errores.append(np.abs(analitica - integrales[1]))
+
+    inicio = time.time()
+    integrales.append(simpson(funcion, a, b, n))
+    tiempos.append(time.time() - inicio)
+    errores.append(np.abs(analitica - integrales[2]))
 
     if (mostrar):
-
-        print("f(x) = {}".format(funcion))
-        print("a = {}\nb = {}\nn = {}".format(a, b, ns))
-        print("∫f(x)dx = {}".format(analitica))
 
         metodos = ["Punto Medio", "Trapeziode", "Simpson"]
-        if (nn > 5): colores = ["purple" for _ in range(nn)]
+        print("f(x) = {}".format(funcion))
+        print("a = {}\nb = {}\nn = {}".format(a, b, n))
+        print("∫f(x)dx = {}".format(analitica))
 
-    for i in range(3):
+        print("---------------------------------------------------")
+        print("                INTEGRACIÓN NUMÉRICA               ")
+        print("---------------------------------------------------")
+        print(" Método\t\tTiempo\tError\t\t∫f(x)dx")
+        print("---------------------------------------------------")
 
-        if (mostrar):
-            
-            print("---------------------------------------------------")
-            print("               REGLA DEL {}              ".format(metodos[i].upper()))
-            print("---------------------------------------------------")
-            print(" n\tTiempo\tError\t\t∫f(x)dx")
-            print("---------------------------------------------------")
-
-        for j in range(nn):
-
-            if (i == 0):
-                inicio = time.time()
-                integral = punto_medio(funcion, a, b, ns[j])
-                tiempo = time.time() - inicio
-
-            elif (i == 1):
-                inicio = time.time()
-                integral = trapezoide(funcion, a, b, ns[j])
-                tiempo = time.time() - inicio
-
-            else:
-                inicio = time.time()
-                integral = simpson(funcion, a, b, ns[j])
-                tiempo = time.time() - inicio
-
-            error = np.abs(analitica - integral)
-
-            if (j == 0):
-                t_metodos += [tiempo]
-                e_metodos += [error]
-                f_metodos += [integral]
-            
-            integrales[i][j], tiempos[i][j], errores[i][j] = integral, tiempo, error
-
-            if (mostrar):
-                print(" {}\t{:.5f}\t{:.2f}\t{}".format(ns[j], tiempo, error, integral))
-
-        if (mostrar):
-            print("---------------------------------------------------\n")
-
-    if (mostrar):
-    
-        print()
-
-        print("-----------------------------------------------------")
-        print("                  INTEGRAL DEFINIDA                  ")
-        print("-----------------------------------------------------")
-        print(" Método\t\tTiempo\tError\t∫f(x)dx")
-        print("-----------------------------------------------------")
         for i in range(3):
-            print(" {}\t{:.5f}\t{:.2f}\t{}".format(metodos[i], t_metodos[i], e_metodos[i], f_metodos[i]))
-        print("-----------------------------------------------------")
+            print(" {}\t{:.5f}\t{:.5f}\t{}".format(metodos[i], tiempos[i], errores[i], integrales[i]))
+
+        print("---------------------------------------------------\n")
 
         f = sym.lambdify(x, funcion)
         t = np.linspace(a, b, 1000)
@@ -329,36 +286,33 @@ def main():
     print("DIFERENCIACIÓN")
 
     print("EJEMPLO 1")
-    funcion = 5*x**4 + 10*x**3 + 15*x**2 + x + 7
-    hs = [1, 0.3, 0.05, 0.007]
-    ejemplo_derivada(funcion, hs, -1, 1, True)
+    funcion = 2*x**4 + 3*x**2 + x
+    hs = [1, 2, 3, 4]
+    ejemplo_derivada(funcion, hs, 0, 3, True)
 
     print("EJEMPLO 2")
-    funcion = 3**(x + 2)
-    hs = [1, 0.4, 0.06, 0.008]
-    ejemplo_derivada(funcion, hs, 2, 5, True)
+    funcion = 3**(x + 2) + x
+    hs = [1, 2, 3, 4]
+    ejemplo_derivada(funcion, hs, 7, 10, True)
 
     print("EJEMPLO 3")
     funcion = sym.sin(2*x**3)
-    hs = [1, 0.2, 0.03, 0.004]
-    ejemplo_derivada(funcion, hs, -0.5, 0.5, True)
+    hs = [0.1, 0.2, 0.3, 0.4]
+    ejemplo_derivada(funcion, hs, 1, 2, True)
 
     print("INTEGRACIÓN")
 
     print("EJEMPLO 1")
-    funcion = 3*sym.cos(x)
-    ns = [1000, 3000, 5000, 7000]
-    ejemplo_integral(funcion, ns, -4.5, 3, True)
+    funcion = 3*x*sym.cos(x)
+    ejemplo_integral(funcion, 1000, -4.5, 3, True)
 
     print("EJEMPLO 2")
     funcion = x**2 + 7*x**3 + 2*x + 1
-    ns = [2000, 4000, 6000, 8000]
-    ejemplo_integral(funcion, ns, -10, 20, True)
+    ejemplo_integral(funcion, 2000, -10, 20, True)
 
     print("EJEMPLO 3")
-    funcion = 2**(-x+5)
-    ns = [1000, 2000, 3000, 4000]
-    ejemplo_integral(funcion, ns, -18.5, -10, True)
+    funcion = 2**(-x + 5)
+    ejemplo_integral(funcion, 3000, -18.5, -10, True)
 
 
-# main()
+main()
